c35a9bc09029370bae4e189c036a46c9c6b70094
Timehin <timehinfarhaat@gmail.com>
2023-06-14 15:58:40 +0100

    coo

--- StringIt/Program.cs
+++ StringIt/Program.cs
@@ -3,7 +3,7 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Hey white");
-        Console.WriteLine("Hey girxsdsdl");
+        Console.WriteLine("Hey gityuyui");
 
     }
 }
