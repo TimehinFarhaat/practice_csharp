5c1e82fadabd7f40ec7407bb8ac2f4cb5e81b7a5
Timehin <timehinfarhaat@gmail.com>
2023-06-14 16:15:59 +0100

    vvvvv

--- StringIt/Program.cs
+++ StringIt/Program.cs
@@ -3,7 +3,7 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Hey white");
-        Console.WriteLine("Hey gityuyugfcsccgdddvdvvdddi");
+        Console.WriteLine("Hey givdddi");
 
     }
 }
